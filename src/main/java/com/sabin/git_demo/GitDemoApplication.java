1843dd1157d866939d09b53c881c222d972b18c3
kcsabin6 <65354977+kcsabin6@users.noreply.github.com>
2020-12-08 20:12:10 -0500

    Say hello endpoint is added in main application

--- src/main/java/com/sabin/git_demo/GitDemoApplication.java
+++ src/main/java/com/sabin/git_demo/GitDemoApplication.java
@@ -2,12 +2,21 @@ package com.sabin.git_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+
+    @GetMapping("/sayhello")
+    public String sayHello() {
+        return "Hello Sabin! How are you?";
+    }
+
 
 }
